22497597e84ba4df3a99f93f9154619dea38eedb
Willy Raedy <wraedy@gmail.com>
2018-01-14 15:02:14 -0600

    refactors as class and adds make spiral functionality with helpers and tests

--- excercise3.js
+++ excercise3.js
@@ -9,41 +9,99 @@ const determineSize = (n) => {
 };
 
 const createEmptySpiral = (size) => {
-  return Array(size).fill(Array(size).fill('  '));
+  const arr = [];
+  for (let i = 0; i < size; i++) {
+    arr.push(Array(size).fill('  '));
+  }
+  return arr;
 };
 
 const getStartingIndex = (size) => {
   return Math.floor(size / 2);
 };
 
-const isNum = (direction, row, col, spiral) => {
-  let rowAdj = 0;
-  let colAdj = 0;
-  switch (direction) {
-    case 'left':
-      colAdj = -1;
-      break;
-    case 'right':
-      colAdj = 1;
-      break;
-    case 'top':
-      rowAdj = -1;
-      break;
-    case 'bottom':
-      rowAdj = 1;
-      break;
-    default:
-      throw new Error('Unrecognized direction');
+class SpiralMaker {
+  constructor(n) {
+    if (n < 0) throw new Error('Number must be a positive integer');
+    const size = determineSize(n);
+    const index = getStartingIndex(size);
+    this.spiral = createEmptySpiral(size);
+    this.row = index;
+    this.col = index;
+    this.target = n;
+
+    this.makeSpiral();
   }
-  // Handle edges. Can't use falsy because 0 is falsy
-  if (spiral[row + rowAdj] === undefined ||
-    spiral[row + rowAdj][col + colAdj] === undefined) return false;
-  return typeof spiral[row + rowAdj][col + colAdj] === 'number';
-};
+
+  isNum(direction) {
+    let rowAdj = 0;
+    let colAdj = 0;
+    switch (direction) {
+      case 'left':
+        colAdj = -1;
+        break;
+      case 'right':
+        colAdj = 1;
+        break;
+      case 'top':
+        rowAdj = -1;
+        break;
+      case 'bottom':
+        rowAdj = 1;
+        break;
+      default:
+        throw new Error('Unrecognized direction');
+    }
+    // Handle edges. Can't use falsy because 0 is falsy
+    if (this.spiral[this.row + rowAdj] === undefined ||
+      this.spiral[this.row + rowAdj][this.col + colAdj] === undefined) return false;
+    return typeof this.spiral[this.row + rowAdj][this.col + colAdj] === 'number';
+  }
+
+  insertNumber(n) {
+    this.spiral[this.row][this.col] = n;
+  }
+
+  goToNextPosition() {
+    if (this.isNum('left')) {
+      if (this.isNum('bottom')) {
+        // go right
+        this.col += 1;
+      } else {
+        // go down
+        this.row += 1;
+      }
+    } else if (this.isNum('right')) {
+      if (this.isNum('top')) {
+        // go left
+        this.col -= 1;
+      } else {
+        // go up
+        this.row -= 1;
+      }
+    } else if (this.isNum('bottom')) {
+      // go right
+      this.col += 1;
+    } else if (this.isNum('top')) {
+      // go left
+      this.col -= 1;
+    } else {
+      // go right
+      this.col += 1;
+    }
+  }
+
+  makeSpiral() {
+    for (let i = 0; i <= this.target; i++) {
+      this.insertNumber(i);
+      this.goToNextPosition(i);
+    }
+  }
+}
 
 module.exports = {
   determineSize,
   createEmptySpiral,
   getStartingIndex,
-  isNum,
+  SpiralMaker,
 };
--- excercise3.spec.js
+++ excercise3.spec.js
@@ -3,10 +3,20 @@ const {
   determineSize,
   createEmptySpiral,
   getStartingIndex,
-  isNum
+  SpiralMaker,
 } = require('./excercise3');
 
 describe('Spiral Maker', () => {
+  const emptySpiral = [
+    ['  ', '  ', '  '],
+    ['  ', '  ', '  '],
+    ['  ', '  ', '  '],
+  ];
+  const centerSpiral = [
+    ['  ', '  ', '  '],
+    ['  ', 10, '  '],
+    ['  ', '  ', '  '],
+  ];
   describe('determineSize', () => {
     it('returns the correct size', () => {
       const num1 = 0;
@@ -23,11 +33,6 @@ describe('Spiral Maker', () => {
 
   describe('empty spiral creator', () => {
     it('returns an empty spiral of the right size', () => {
-      const emptySpiral = [
-        ['  ', '  ', '  '],
-        ['  ', '  ', '  '],
-        ['  ', '  ', '  '],
-      ];
       expect(createEmptySpiral(3)).to.deep.equal(emptySpiral);
     });
   });
@@ -41,26 +46,83 @@ describe('Spiral Maker', () => {
   });
 
   describe('isNum', () => {
-    const spiral = [
-      ['  ', '  ', '  '],
-      ['  ', 10, '  '],
-      ['  ', '  ', '  '],
-    ];
+    const spiralMaker = new SpiralMaker(8);
+    spiralMaker.spiral = centerSpiral;
     it('determines if there is a number to the left', () => {
-      expect(isNum('left', 1, 2, spiral)).to.equal(true);
-      expect(isNum('left', 2, 2, spiral)).to.equal(false);
+      spiralMaker.row = 1;
+      spiralMaker.col = 2;
+      expect(spiralMaker.isNum('left')).to.equal(true);
+      spiralMaker.row = 2;
+      spiralMaker.col = 2;
+      expect(spiralMaker.isNum('left')).to.equal(false);
     });
     it('determines if there is a number to the right', () => {
-      expect(isNum('right', 1, 0, spiral)).to.equal(true);
-      expect(isNum('right', 2, 2, spiral)).to.equal(false);
+      spiralMaker.row = 1;
+      spiralMaker.col = 0;
+      expect(spiralMaker.isNum('right')).to.equal(true);
+      spiralMaker.row = 2;
+      spiralMaker.col = 2;
+      expect(spiralMaker.isNum('right')).to.equal(false);
     });
     it('determines if there is a number to the top', () => {
-      expect(isNum('top', 2, 1, spiral)).to.equal(true);
-      expect(isNum('top', 2, 2, spiral)).to.equal(false);
+      spiralMaker.row = 2;
+      spiralMaker.col = 1;
+      expect(spiralMaker.isNum('top')).to.equal(true);
+      spiralMaker.row = 2;
+      spiralMaker.col = 2;
+      expect(spiralMaker.isNum('top')).to.equal(false);
     });
     it('determines if there is a number to the left', () => {
-      expect(isNum('bottom', 0, 1, spiral)).to.equal(true);
-      expect(isNum('bottom', 2, 2, spiral)).to.equal(false);
+      spiralMaker.row = 0;
+      spiralMaker.col = 1;
+      expect(spiralMaker.isNum('bottom')).to.equal(true);
+      spiralMaker.row = 2;
+      spiralMaker.col = 2;
+      expect(spiralMaker.isNum('bottom')).to.equal(false);
+    });
+  });
+
+  describe('insert number', () => {
+    it('inserts number in the current position', () => {
+      const spiralMaker = new SpiralMaker(8);
+      spiralMaker.row = 1;
+      spiralMaker.col = 1;
+      spiralMaker.insertNumber(99);
+      expect(spiralMaker.spiral[1][1]).to.equal(99);
+      spiralMaker.row = 0;
+      spiralMaker.col = 0;
+      spiralMaker.insertNumber(66);
+      expect(spiralMaker.spiral[0][0]).to.equal(66);
+    });
+  });
+
+  describe('place next number', () => {
+    it('places numbers correctly', () => {
+      const spiralMaker = new SpiralMaker(8);
+      spiralMaker.spiral = emptySpiral;
+      spiralMaker.row = 1;
+      spiralMaker.col = 1;
+      spiralMaker.goToNextPosition();
+      expect(spiralMaker.row).to.equal(1);
+      expect(spiralMaker.col).to.equal(2);
+      spiralMaker.spiral = centerSpiral;
+      spiralMaker.row = 1;
+      spiralMaker.col = 2;
+      spiralMaker.goToNextPosition();
+      expect(spiralMaker.row).to.equal(2);
+      expect(spiralMaker.col).to.equal(2);
+    });
+  });
+
+  describe('make spiral', () => {
+    it('makes spiral correctly', () => {
+      const resultFor8 = [
+        [6, 7, 8],
+        [5, 0, 1],
+        [4, 3, 2],
+      ];
+      const spiralMaker = new SpiralMaker(8);
+      expect(spiralMaker.spiral).to.deep.equal(resultFor8);
     });
   });
 });
